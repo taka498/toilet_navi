5a4ae9e791765bad0b76e4d995fa3419b846b5d9
taka498 <y.ktcae5e4d3@gmail.com>
2026-01-26 22:24:38 +0900

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # トップページ
+  root "pages#home"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # 静的ページ
+  get "/terms",   to: "pages#terms"
+  get "/privacy", to: "pages#privacy"
+  get "/contact", to: "pages#contact"
+
+  # トイレ表示画面
+  get "/search", to: "search#new"
+
+  # （将来）ログイン・サインアップ
+  # get "/login",  to: "sessions#new"
+  # get "/signup", to: "users#new"
 end
